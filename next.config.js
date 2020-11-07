3f094388512236bae57f774b6e0e2fc0ceca07bc
Craig bauer <craigbauer23@gmail.com>
2020-11-07 11:32:37 -0500

    updated css and config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ module.exports = {
 /*	devIndicators: {
 	    autoPrerender: false,
 	},*/
-	target: 'serverless',
-	webpack: config => {
-		config.plugins.push(
-			new webpack.DefinePlugin({ 'global.GENTLY': false }),
-		);
-		return config;
-	},
 	sassOptions: {
 	  	//cssModules: true,
 		includePaths: [path.join(__dirname, 'scss')],
